6a5f0ed8bcfe3091d0e1476a1a025493737881fa
markgloshen <mark@gloshen.com>
2022-03-09 13:39:20 -0800

    Add OOP scripts
    Add the scripts related Object Oriented Programming.
    
    Define Animal base class and 3 inherited classes that exemplify
    the four pillars of OOP: Inheritence, Polymorphism, Encapsulation,
    and Abstraction.

--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -1,18 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Horse : MonoBehaviour
+public class Horse : Animal // INHERITANCE
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _numLegs = 4;
+
+    // Must override the abstract Properties from base class Animal.
+    public override int NumLegs // POLYMORPHISM
     {
-        
+        get => _numLegs;
+        protected set => _numLegs = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Use protected setter from base class to set name and speed.
+    private void Awake()
     {
-        
+        Name = "Horse";
+        Speed = 10f;
     }
 }
